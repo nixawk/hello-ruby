b743db7b96a1a8d513fdde7a1b9540e5d7eb9c20
nixawk <hap.ddup@gmail.com>
2015-11-23 15:17:20 +0000

    ruby pseudo variables

---
+++ basic/ruby_pseudo_variables.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/ruby
+
+=begin
+
+ self: The receiver object of the current method.
+ true: Value representing true.
+ false: Value representing false.
+ nil: Value representing undefined.
+ __FILE__: The name of the current source file.
+ __LINE__: The current line number in the source file.
+
+=end
+
+puts __FILE__
+puts __LINE__
