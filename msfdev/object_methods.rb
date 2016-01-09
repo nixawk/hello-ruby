6682010d2436ec0d26073c366b8f3d2d28ee4546
nixawk <hap.ddup@gmail.com>
2016-01-09 17:34:55 +0800

    class method / instance method

--- msfdev/object_methods.rb
+++ msfdev/object_methods.rb
@@ -2,3 +2,10 @@
 # What's the differences ?
 Rex::Socket.methods
 Rex::Socket#methods
+
+# Within the text, Fred#doIt is a reference to an instance method (doIt) of
+# class, while Fred.new [In some other Ruby documentation, you may see class
+# methods written as Fred::new. This is perfectly valid Ruby syntax; we just
+# happen to feel that Fred.new is less distracting to read.] is a class
+# method, and Fred::EOF is a class constant.
+
