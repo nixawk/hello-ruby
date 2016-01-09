03720abd528b49823687ce351c877d3680d93e89
nixawk <hap.ddup@gmail.com>
2016-01-09 22:01:58 +0800

    ruby collections - arrays and hashes

---
+++ basic/ruby_arrays_and_hashes.rb
@@ -0,0 +1,8 @@
+
+# Ruby's array and hashes are indexed collections. Both store collections of
+# objects, accessible using a key. With arrays, the key is an integer, whereas
+# hashes support any object as a key. Both arrays and hashes groe as needed to
+# provide more flexibility. It's more efficient to access array elements, but
+# hashes provide more flexibility. Any particular array or hash can
+# hold objects of differing types; you can have an array containing an
+# integer, a string, and a floating point number, as we'll see in a minute.
