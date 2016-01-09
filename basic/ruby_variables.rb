be52064daf40e8dc7e8256e2d545d4fa79cfde2e
nixawk <hap.ddup@gmail.com>
2016-01-09 21:50:49 +0800

    ruby variables

---
+++ basic/ruby_variables.rb
@@ -0,0 +1,9 @@
+
+# Ruby uses a convention to help it distinguish the usage of a name:
+# the first characters of a name indicate how the name is used.
+# - Local variables, method parameters, and method names should all start with
+#   a lowercase letter or with an underscore.
+# - Global variables are prefixed with a dollar sign ($), while instance
+#   variables begin with an ``at''(@).
+# - Class variables start with two ``at'' signs (@@).
+#
