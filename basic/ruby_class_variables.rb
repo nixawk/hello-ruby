17d5872aabd3a11e36b1d5451feedd3aa092d648
nixawk <hap.ddup@gmail.com>
2016-01-10 19:00:09 +0800

    ruby class variables

--- basic/ruby_class_variables.rb
+++ basic/ruby_class_variables.rb
@@ -3,6 +3,15 @@
 # Class variables begin with @@
 # and must be initialized before they can be used in method definitions.
 
+# A Class variable is shared among all objects of a class, and it is also
+# accessible to the class methods that we'll describe later. There is only one
+# copy of a particular class variable for a given class.
+
+# Class variable names start with two ``at'' signs, such as ``@@count''.
+# Unlike global and instance variables, class variables must be initialized
+# before they are used. Often this initialization is just a simple assignment
+# in the body of the class definition.
+
 class Customer
   @@number = 0
   def initialize(id, name, addr)
@@ -33,3 +42,8 @@ cust2.display_details
 
 cust1.total_number
 cust2.total_number
+
+# Class variables are private to a class and its instance. If you want to make
+# them accessible to the outside world, you'll need to write an accessor
+# method. This method could be either an instance method or, leading us neatly
+# to the next section, a class method.
