64271a21aaa0dcdeb23c4f02f53d513b283b566e
nixawk <hap.ddup@gmail.com>
2016-01-10 20:55:57 +0800

    ruby variables

--- basic/ruby_variables.rb
+++ basic/ruby_variables.rb
@@ -7,3 +7,24 @@
 #   variables begin with an ``at''(@).
 # - Class variables start with two ``at'' signs (@@).
 #
+
+# Variables are used to keep track of objects; each variable holds a reference
+# to an object.
+
+person = 'Tim'
+puts "id: #{person.__id__}"
+puts "class: #{person.class}"
+puts persion
+
+# Is a variable an object ?
+# In Ruby, the answer is ``no''. A variable is simply a reference to an object.
+
+person1 = person
+person[0] = 'J'
+puts person
+puts person1
+
+# What happened here ? We changed the first character of person1, but both
+# person1 and person2 changed from ``Tim'' to ``Jim''
+# It all comes back to that variables hold references to objects, not the
+# objects themselves.
