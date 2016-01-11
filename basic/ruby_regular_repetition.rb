51689ef0960d3790d8c3bd4d0548d88def81d1f6
nixawk <hap.ddup@gmail.com>
2016-01-11 21:26:12 +0800

    ruby regular repetition

---
+++ basic/ruby_regular_repetition.rb
@@ -0,0 +1,8 @@
+
+# If r stands for the immediately preceding regular expression within a
+# pattern then:
+#  r *       - matches zero or more occurrences of r,
+#  r +       - matches one or more occurrences of r,
+#  r ?       - matches zero or one occurrence of r,
+#  r {m, n}  - matches at least ``m'' and at most ``n'' occurrences of r.
+#  r {m, }   - matches at least ``m'' occurrences of r.
