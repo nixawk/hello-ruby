40abda60b9520e33cfb78774c0e8f741ba25aa0e
nixawk <hap.ddup@gmail.com>
2017-04-20 04:50:01 -0500

    Fix ruby class methods desc

--- basic/ruby_class_methods.rb
+++ basic/ruby_class_methods.rb
@@ -3,11 +3,15 @@
 # Class methods are defined by placing the class name and a period in front of
 # the method name.
 
+# Class methods are distinguished from instance methods by their definition.
+# Class methods are defined by placing the class name and a period in front of the method name.
+
 class Example
   def instanceMethod
     puts "This is an instance method"
   end
 
+  # Sometimes a class needs to provide methods that work without being tied to any particular object.
   def Example.classMethod
     puts "This is a class method"
   end
