faa4adeeb5dd965c71cbfb31c12db684331cfcc4
nixawk <hap.ddup@gmail.com>
2017-04-21 05:32:06 -0500

    add attr_accessor

--- basic/ruby_[attr_accessor]_[attr_reader]_[attr_writer].rb
+++ basic/ruby_[attr_accessor]_[attr_reader]_[attr_writer].rb
@@ -6,14 +6,14 @@
 # attr_writer: var
 
 class Person
-  attr_writer :age
+  ## attr_writer :age
 
   # /* same as follow code*/
   # def age=(value)
   #   @age = value
   # end
 
-  attr_reader :age
+  ## attr_reader :age
 
   # /* same as follow code*/
   # def age
@@ -21,5 +21,10 @@ class Person
   # end
 
   # attr_accessor :age = attr_reader :age + attr_writer :age
-
+  attr_accessor :age
 end
+
+
+jack = Person.new
+jack.age = 25
+puts jack.age
