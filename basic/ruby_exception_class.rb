a505ba138816be86762018b824767e409f167737
nixawk <hap.ddup@gmail.com>
2017-04-22 21:10:36 -0500

    Fix desc

--- basic/ruby_exception_class.rb
+++ basic/ruby_exception_class.rb
@@ -1,10 +1,14 @@
 
+## The Exception Class
+
 # The package that contains the information about an exception is an object of
 # class Exception, or one of class Exception'S children. Ruby predefines a
 # tidy hierarchy of exceptions.
 #
 # When you need to raise an exception, you can use one of the built-in
-# Exception classes, or you can create one of your own.
+# Exception classes, or you can create one of your own. If you create your
+# own, you might want to make it a subclass of StandardError or one of
+# its children. If you don't, your exception won't be caught by default.
 
 
 begin
