4e30cd70633cf0523db4a816dc02fbe224190402
nixawk <hap.ddup@gmail.com>
2017-04-23 08:21:58 -0500

    add Dir object methods

---
+++ basic/ruby_Dir.creating_a_directory.rb
@@ -0,0 +1,14 @@
+
+## Dir
+
+puts Dir.getwd
+puts Dir.pwd
+
+# Creating a Directory
+Dirs.mkdir("newdir")
+
+# Deleting a Directory
+Dirs.delete("newdir")
+
+# If you want more, please 
+# Dir.methods.each { |method| puts method }
